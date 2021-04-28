5e0e9c944466181d7a88fdbe39a43a907700abf7
Jaime Nebrera <jnebrera@gmail.com>
2021-04-28 20:18:42 +0200

    (A)mateur (B)asketball (T)racking (S)ystem

--- abts.py
+++ abts.py
@@ -182,4 +182,3 @@ cv2.destroyAllWindows()
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
